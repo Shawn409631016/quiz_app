17a8b9903419cc272686cb0633697ffd45519804
Arthur45440809 <Arthur45440809@gmail.com>
2022-11-24 22:29:28 +0800

    finish_quiz

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,32 @@
-import '../styles/App.css';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import "../styles/App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 /** import components */
-import Main from './Main';
-import Quiz from './Quiz';
-import React from 'react';
+import Main from "./Main";
+import Quiz from "./Quiz";
+import React from "react";
 /** react routers */
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Main></Main>,
-	},
-	{
-		path: '/quiz',
-		element: <Quiz></Quiz>,
-	},
-	{
-		path: '/result',
-		element: <Quiz> </Quiz>,
-	},
+  {
+    path: "/",
+    element: <Main></Main>,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz></Quiz>,
+  },
+  {
+    path: "/result",
+    element: <Quiz> </Quiz>,
+  },
 ]);
 
 function App() {
-	return (
-		<>
-			<RouterProvider router={router} />
-		</>
-	);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
