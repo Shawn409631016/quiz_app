cf85f81178639d7a82ce8c9ab14cfabca80ea317
shawn <shawnfeng610@gmail.com>
2022-11-25 22:58:19 +0800

    starting push user result

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,24 +1,38 @@
 import React, { useEffect } from 'react';
 import Questions from './Questions';
 
+import { MoveNextQuestion, MovePrevQuestion } from '../hooks/FetchQuestion';
+
 /** redux store import */
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function Quiz() {
-	const state = useSelector((state) => state);
+	// const trace = useSelector((state) => state.questions.trace);
+	const { queue, trace } = useSelector((state) => state.questions);
+	const dispatch = useDispatch();
 
 	useEffect(() => {
-		console.log(state);
+		console.log(trace);
 	});
 
 	/*next button event handler*/
 	function onNext() {
 		console.log('On next click');
+
+		if (trace < queue.length) {
+			/**update the trace value by one using MoveNextAction */
+			dispatch(MoveNextQuestion());
+		}
 	}
 
 	/*Prev button event handler*/
 	function onPrev() {
 		console.log('On Prev click');
+
+		if (trace > 0) {
+			/**update the trace value by one using MovePrevAction */
+			dispatch(MovePrevQuestion());
+		}
 	}
 
 	return (
